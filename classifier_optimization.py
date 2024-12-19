eb8d1e36196ba574958d79f14a9f1520bd7f4ce1
César Camus-Emschwiller <115382806+CesarCams@users.noreply.github.com>
2024-12-19 15:34:15 +0100

    Update classifier_optimization.py

--- classifier_optimization.py
+++ classifier_optimization.py
@@ -98,7 +98,7 @@ elif classifier_type == "MLPClassifier":
             return self.model(x)
 
     hidden_layer_sizes = [50, 100, 200] 
-    learning_rates = [0.00001, 0.001, 0.1] 
+    learning_rates = [0.00001,0.0001, 0.001, 0.01, 0.1, 1] 
     epoch_values = [100] 
 
     best_f1 = 0
